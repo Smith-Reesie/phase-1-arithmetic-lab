e9cb4e91a594b877a0314f2ba7e7af0274294dcb
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-13 11:43:37 -0400

    commit tree 55b949f97f276ce4b629499785c6990ac0f9d813

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = (num1*num2);
+    
+
+Number.isInteger(random)
+let random = (0);
+++random;
+
+const num3 = 2
+const num4 = 2
+let mod = (num3+num2);
+
+let max = 20
+
+Math.max(18,19,20);
